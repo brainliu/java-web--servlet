3ef36b885515d1083af7f67e05311bfd58ee8e3a
brainliu <406096277@qq.com>
2016-08-25 17:56:05 +0800

    src
    this is the java servlet class

---
+++ login.java
@@ -0,0 +1,40 @@
+package cn.web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class login
+ */
+public class login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf-8");
+		String username=request.getParameter("username");
+		String userpwd=request.getParameter("userpwd");
+		if("eric".equals(username)
+				&&"123456".equals(userpwd)){
+			HttpSession session=request.getSession();
+			session.setAttribute("loginname", username);
+			response.sendRedirect(request.getContextPath()+"/index");	
+		}else{
+			response.sendRedirect(request.getContextPath()+"/fail.html");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
