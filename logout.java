2ebf58db3eef804027f32008a842c59f480fa824
brainliu <406096277@qq.com>
2016-08-25 17:57:33 +0800

    src

---
+++ logout.java
@@ -0,0 +1,31 @@
+package cn.web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+public class logout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		HttpSession session=request.getSession(false);
+		if(session!=null){
+			session.removeAttribute("loginname");
+		}
+		response.sendRedirect(request.getContextPath()+"/login.html");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
